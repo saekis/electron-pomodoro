d4678946e08f829aa6aca56266af113f37619747
Shohei Saeki <s.saeki71@gmail.com>
2017-01-25 21:14:19 +0900

    implement timer

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const {app, Menu, Tray, BrowserWindow} = require('electron')
+const {app, Menu, Tray, BrowserWindow, ipcMain} = require('electron')
 const dateFormat = require("dateformat")
 const path = require('path')
 
 let tray = undefined
 let window = undefined
+let timer = undefined
+let datetime = undefined
+let timer_type = 'work'
+let timer_status = 'stop'
 
 // Hide icon from dock menu
 app.dock.hide()
@@ -14,11 +18,12 @@ app.on('ready', () => {
 })
 
 const createTray = () => {
-  tray = new Tray(__dirname + '/images/icon.png')
-  tray.setToolTip('pomodoro^^')
+  tray = new Tray(__dirname + '/images/tomato.png')
+  tray.setToolTip('pomodoro')
 
-  const callback = () => {}
-  const timer = startTimer(tray, callback, 25, 0)
+  const initWorkTime = formatTime(25, 0)
+  tray.setTitle(initWorkTime)
+  setDateTime(25, 0)
 
   tray.on('click', toggleWindow)
 }
@@ -26,8 +31,10 @@ const createTray = () => {
 const toggleWindow = () => {
   if (window.isVisible()) {
     window.hide()
+    tray.setHighlightMode('never')
   } else {
     showWindow()
+    tray.setHighlightMode('always')
   }
 }
 
@@ -41,17 +48,17 @@ const createWindow = () => {
     resizable: false,
     transparent: true,
     webPreferences: {
-      // Prevents renderer process code from not running when window is
-      // hidden
       backgroundThrottling: false
     }
   })
   window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
+  // window.toggleDevTools()
+
   // Hide the window when it loses focus
   window.on('blur', () => {
     if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
+      toggleWindow()
     }
   })
 }
@@ -66,7 +73,7 @@ const getWindowPosition = () => {
   // Position window 4 pixels vertically below the tray icon
   const y = Math.round(trayBounds.y + trayBounds.height + 4)
 
-  return {x: x, y: y}
+  return {x, y}
 }
 
 const showWindow = () => {
@@ -76,22 +83,65 @@ const showWindow = () => {
   window.focus()
 }
 
-const startTimer = (tray, callback, m, s) => {
-  // Initiallize Date object
-  let time = new Date(null, null, null, null, m, s)
-
-  const start_time = dateFormat(time, 'MM:ss')
-  let displaying_time = start_time
-  tray.setTitle(displaying_time);
-
-  const timer = setInterval(() => {
-    time.setSeconds(time.getSeconds() - 1);
-    displaying_time = dateFormat(time, 'MM:ss')
-    tray.setTitle(displaying_time);
+const startTimer = (m, s) => {
+  datetime = new Date(null, null, null, null, m, s)
+  timer = setInterval(() => {
+    datetime.setSeconds(datetime.getSeconds() - 1);
+    const displaying_time = dateFormat(datetime, 'MM:ss')
+    tray.setTitle(displaying_time)
     if (displaying_time == '00:00') {
-      callback()
-      clearInterval(timer);
+      clearInterval(timer)
     }
   }, 1000)
-  return timer
 }
+
+const formatTime = (m, s) => {
+  const time = new Date(null, null, null, null, m, s)
+  return dateFormat(time, 'MM:ss')
+}
+
+const startWorkTime = () => {
+}
+
+const startBreakTime = (m, s) => {
+}
+
+const stopWorkTime = () => {
+
+}
+
+const stopBreakTime = () => {
+
+}
+
+const setDateTime = (m, s) => {
+  datetime = new Date(null, null, null, null, m, s)
+}
+
+const setTimerStatus = (status) => {
+  timer_status = status
+}
+
+const setTimerType = (type) => {
+  timer_type = type
+}
+
+ipcMain.on('start-timer', (event, arg) => {
+  startTimer(datetime.getMinutes(), datetime.getSeconds())
+  toggleWindow()
+  setTimerStatus('progress')
+})
+
+ipcMain.on('stop-timer', (event, arg) => {
+  clearInterval(timer)
+  toggleWindow()
+  setTimerStatus('stop')
+});
+
+ipcMain.on('timer-type', (event, arg) => {
+  event.sender.send('asynchronous-reply', timer_type);
+});
+
+ipcMain.on('timer-status', (event, arg) => {
+  event.sender.send('asynchronous-reply', timer_status);
+});
---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Menu from './menu'
+
+ReactDOM.render(<Menu />, document.getElementById('react-window'))
